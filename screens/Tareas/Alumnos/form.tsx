baf08eb615cea67a9d1c13d83bde471b9793c1b7
Erick Hernandez <takashi.onimaru@gmail.com>
2022-06-25 23:12:49 -0400

    Form con Calendar

--- screens/Tareas/Alumnos/form.tsx
+++ screens/Tareas/Alumnos/form.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Wrapper from '../../../components/Wrapper'
-import { Text } from '../../../components/Themed'
+import { ScrollView, View, TouchableOpacity } from 'react-native'
 import styles from '../style'
+import { DateTimePickerAndroid } from '@react-native-community/datetimepicker'
+import { TextInput } from 'react-native-paper'
+import Moment from 'moment'
 
 const FormAlumnos = (props: any) => {
   const { navigation } = props;
+  const [date, setDate] = useState(new Date());
+  const [fecha, setFecha] = useState('');
+  Moment.locale('es-ve');
+
   const save = () => {
 
   }
+
+  useEffect(() => {
+
+    setFecha(Moment(date).format('DD/MM/YYYY'))
+  }, [date])
+
+  const onChange = (event: any, selectedDate: any) => {
+    const currentDate = selectedDate;
+    setDate(currentDate);
+  }
+
+  const showDatepicker = () => {
+    DateTimePickerAndroid.open({
+      value: date,
+      onChange,
+      mode: 'date',
+      display:'calendar'
+    })
+  }
+
   return (
     <Wrapper
       title={'Form Alumnos'}
@@ -16,7 +43,46 @@ const FormAlumnos = (props: any) => {
       back={true}
       save={save}
     >
-      <Text style={styles.title}>Alumnos form content</Text>
+      <ScrollView>
+        <View>
+          <TextInput
+            label="Nombre"
+            mode='outlined'
+            autoComplete='name'
+          />
+
+          <TextInput
+            label="Apellido"
+            mode='outlined'
+            autoComplete='name-family'
+          />
+
+          <TextInput
+            label="Correo"
+            mode='outlined'
+            autoComplete='email'
+            keyboardType='email-address'
+          />
+
+          <TextInput
+            label="Teléfono"
+            mode='outlined'
+            autoComplete='tel'
+            keyboardType='phone-pad'
+          />
+          <TouchableOpacity onPress={() =>{showDatepicker()}}>
+            <TextInput
+              label="Fecha de Nacimiento"
+              mode='outlined'
+              editable={false}
+              value={fecha}
+              onPressIn={() => { showDatepicker() }}
+              right={<TextInput.Icon name="calendar" />}
+            />
+          </TouchableOpacity>
+
+        </View>
+      </ScrollView>
     </Wrapper>
   )
 }
