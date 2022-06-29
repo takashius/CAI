e892159687fb4213c7eab39f018e3f92871d3445
Erick Hernandez <takashi.onimaru@gmail.com>
2022-06-29 13:05:25 -0400

    Pre deploy

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import * as Font from 'expo-font';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -10,21 +9,8 @@ import Navigation from './navigation';
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  const [isFontLoaded , setFontLoaded ] = useState<boolean>(false);
 
-  useEffect(()=> {
-    loadFonts();
-  }, [] )
-
-  const loadFonts = async () => {
-    await Font.loadAsync({
-      'RobotoBold' : require('./assets/fonts/Roboto-Bold.ttf'),
-      'RobotoRegular' : require('./assets/fonts/Roboto-Regular.ttf'),
-    });
-    setFontLoaded(true);
-  }
-
-  if (!isLoadingComplete || !isFontLoaded) {
+  if (!isLoadingComplete) {
     return null;
   } else {
     return (
--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -16,6 +16,8 @@ export default function useCachedResources() {
         await Font.loadAsync({
           ...FontAwesome.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          'RobotoBold' : require('../assets/fonts/Roboto-Bold.ttf'),
+          'RobotoRegular' : require('../assets/fonts/Roboto-Regular.ttf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
